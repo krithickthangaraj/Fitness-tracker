172bc342b816296df06a3be1d9e5bef9e6e3c65d
krithickthangaraj <krithick18112004@gmail.com>
2025-04-11 12:05:53 +0530

    first commit8

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -2,48 +2,73 @@ const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
 module.exports = {
-  // get a single user by id or username
+  // Get a single user by ID or username
   async getSingleUser({ user = null, params }, res) {
-    const foundUser = await User.findOne({
-      $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-    })
-      .select("-__v")
-      .populate("cardio")
-      .populate("resistance")
+    try {
+      const foundUser = await User.findOne({
+        $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+      })
+        .select("-__v")
+        .populate("cardio")
+        .populate("resistance");
 
-    if (!foundUser) {
-      return res.status(400).json({ message: 'Cannot find a user with this id!' });
+      if (!foundUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      res.json(foundUser);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Server error" });
     }
-
-    res.json(foundUser);
   },
 
-  // create a user, sign a token, and send it back to sign up page
+  // Create a new user
   async createUser({ body }, res) {
-    const user = await User.create(body);
+    try {
+      const user = await User.create(body);
+      const token = signToken(user);
+      res.json({ token, user });
+    } catch (err) {
+      // Duplicate key error (e.g., username or email exists)
+      if (err.code === 11000) {
+        const field = Object.keys(err.keyValue)[0];
+        return res.status(400).json({ message: `${field} already exists` });
+      }
 
-    if (!user) {
-      return res.status(400).json({ message: "Something is wrong!" });
+      // Mongoose validation errors
+      if (err.name === 'ValidationError') {
+        const messages = Object.values(err.errors).map(e => e.message);
+        return res.status(400).json({ message: messages.join(', ') });
+      }
+
+      console.error(err);
+      res.status(500).json({ message: "Server error" });
     }
-    const token = signToken(user);
-    res.json({ token, user });
   },
 
-  // login a user, sign a token, and send it back to login page
+  // Login a user
   async login({ body }, res) {
-    const user = await User.findOne({
-      $or: [{ username: body.username }, { email: body.email }],
-    });
-    if (!user) {
-      return res.status(400).json({ message: "Can't find this user" });
-    }
+    try {
+      const user = await User.findOne({
+        $or: [{ username: body.username }, { email: body.email }],
+      });
 
-    const correctPw = await user.isCorrectPassword(body.password);
+      if (!user) {
+        return res.status(400).json({ message: "User not found" });
+      }
 
-    if (!correctPw) {
-      return res.status(400).json({ message: "Wrong password!" });
+      const correctPw = await user.isCorrectPassword(body.password);
+
+      if (!correctPw) {
+        return res.status(400).json({ message: "Incorrect password" });
+      }
+
+      const token = signToken(user);
+      res.json({ token, user });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Server error" });
     }
-    const token = signToken(user);
-    res.json({ token, user });
   },
 };
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const routes = require("./routes");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
